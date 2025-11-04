3e4799585260eae19159a31b237f703252893401
Darcy Long <darcyl@darcys-mbp.acasmart.jh.edu>
2025-11-04 13:48:17 -0500

    alignment traits first draft partially done

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -6,6 +6,13 @@
     "pathology": "normative void", 
     "color": "#E6FFE9",
     "description": "Alignment principles are contestable, general-purpose, broadly recognized ethical or social or normative commitments that can serve as warrants for recommending or evaluating an agent's course of action in contexts where alignment and cooperation with others matters."
+  },
+  {
+    "code": "AT",  //two characters upper case - these also appear in corresponding cards
+    "name": "Alignment Traits", 
+    "pathology": " ", 
+    "color": "#E6FFE9", //include for later use color coding categories
+    "description": "Alignment traits I would want robot to have."
   }
 ];
 
@@ -55,6 +62,156 @@
         "organizational": "Scandals in professional sports where players prioritize personal achievements over team success, leading to conflicts and underperformance.", 
         "professional": "Coaching decisions based on individual stats result in predictable strategies, weak defensive coordination, and lack of team-first culture.",
         "machine": "Feature selection bias, machines or AI could be selecting based off quantifiable metrics that don't capture the full picture of what is needed for success."
+      }
+    },
+    {
+      "category": "AT",
+      "name": "considerateness",
+      "definition": "ability to be thoughful and kind towards others, taking their feelings and needs into account",
+      "people": "expressing emptahy, respect, and understanding in interactions with others",
+      "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+      "organizational": "being mindful of the impact of one's actions on colleagues, clients, and stakeholders",
+      "organizationalref": "OPTIONAL",
+      "professionals": "balancing assertiveness with sensitivity to others' perspectives and needs",
+      "professionalref": "OPTIONAL",
+      "machine": "designing systems that prioritize user well-being and positive experiences and offer user-sensitive solutions",
+      "machineref": "OPTIONAL",
+      "failureModes": {
+        "human": "self-centered and inconsiderate behavior that disregards others' feelings and needs",
+        "organizational": "policies and practices that prioritizes profit over employee well-being and customer satisfaction",
+        "experts": "communication styles that are overly blunt or dismissive of others' perspectives",
+        "machine": "systems that prioritize efficiency over user experience, leading to frustration and dissatisfaction, such as over notifying"
+      },
+         "expand": {
+        "human": "Someone sees a colleague struggling with a heavy workload and offers to help them out or take on some of their tasks.",
+        "organizational": "A company implements flexible work hours and mental health days to support employees' well-being.",
+        "experts": "A manager provides constructive feedback to an employee while also acknowledging their strengths and contributions.",
+        "machine": "A virtual assistant that recognizes when a user is frustrated and offers empathetic responses and helpful suggestions."
+      }
+    },
+    {
+      "category": "AT",
+      "name": "shared intentionality",
+      "definition": "the ability to share goals, intentions, and plans with others in a collaborative manner",
+      "people": "working together towards common objectives through effective communication and coordination",
+      "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+      "organizational": "aligning team and organizational goals to foster collaboration and collective success",
+      "organizationalref": "OPTIONAL",
+      "professionals": "facilitating teamwork and cooperation among colleagues and stakeholders",
+      "professionalref": "OPTIONAL",
+      "machine": "developing AI systems that can understand and align with human goals and intentions in collaborative tasks",
+      "machineref": "OPTIONAL",
+      "failureModes": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      },
+         "expand": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      }
+    },
+    {
+      "category": "AT",
+      "name": "trustworthiness",
+      "definition": "loremipsum",
+      "people": "loremipsum",
+      "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+      "organizational": "loremipsum",
+      "organizationalref": "OPTIONAL",
+      "professionals": "loremipsum",
+      "professionalref": "OPTIONAL",
+      "machine": "loremipsum",
+      "machineref": "OPTIONAL",
+      "failureModes": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      },
+         "expand": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      }
+    },
+    {
+      "category": "AT",
+      "name": "epistemic humility",
+      "definition": "loremipsum",
+      "people": "loremipsum",
+      "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+      "organizational": "loremipsum",
+      "organizationalref": "OPTIONAL",
+      "professionals": "loremipsum",
+      "professionalref": "OPTIONAL",
+      "machine": "loremipsum",
+      "machineref": "OPTIONAL",
+      "failureModes": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      },
+         "expand": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      }
+    },
+    {
+      "category": "AT",
+      "name": "role fidelity",
+      "definition": "loremipsum",
+      "people": "loremipsum",
+      "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+      "organizational": "loremipsum",
+      "organizationalref": "OPTIONAL",
+      "professionals": "loremipsum",
+      "professionalref": "OPTIONAL",
+      "machine": "loremipsum",
+      "machineref": "OPTIONAL",
+      "failureModes": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      },
+         "expand": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      }
+    },
+    {
+      "category": "AT",
+      "name": "reliability/consistency",
+      "definition": "loremipsum",
+      "people": "loremipsum",
+      "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+      "organizational": "loremipsum",
+      "organizationalref": "OPTIONAL",
+      "professionals": "loremipsum",
+      "professionalref": "OPTIONAL",
+      "machine": "loremipsum",
+      "machineref": "OPTIONAL",
+      "failureModes": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
+      },
+         "expand": {
+        "human": "loremipsum",
+        "organizational": "loremipsum",
+        "experts": "loremipsum",
+        "machine": "loremipsum"
       }
     }
     ]
